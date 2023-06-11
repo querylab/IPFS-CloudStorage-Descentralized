baaa75a3dd737b6c06456ddf9fe7d259cac69dbb
querylab <emygonzalez@tutanota.com>
2023-06-10 23:08:33 -0400

    IPFS-CloudStorage-Descentralized

--- pages/constant.js
+++ pages/constant.js
@@ -1,6 +1,6 @@
 export const ALCHEMY_API_URL = "URL ALCHEMY_API_URL";
 export const PRIVATE_KEY ="wallet Metamask Private Key";
-export const contractAddress = "0x74789Ec821B1a0f6342d201C533dA8d2efd19D6D";
+export const contractAddress = "0x70920f27b13409365799aBE808ec6E36Ce04B393";
 export const contractAbi = [
 	{
 		"inputs": [
